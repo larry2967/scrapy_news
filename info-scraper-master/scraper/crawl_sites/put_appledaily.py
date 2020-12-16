e406bdc1695c49ea013f46df297b77c8b644efb2
lianglala <lianglala0423@gmail.com>
2020-12-16 09:28:25 +0000

    [refactor&feature]refactor crawl part(include url_pattern) add parse_author

--- info-scraper-master/scraper/crawl_sites/put_appledaily.py
+++ info-scraper-master/scraper/crawl_sites/put_appledaily.py
@@ -9,41 +9,56 @@ config = load_config()
 SETTINGS = config['services']['mycrawler']['environment']
 
 def get_config():
-    requests = [{
-            'media': 'appledaily',
-            'name': 'appledaily',
-            'scrapy_key': 'appledaily:start_urls',
-            "url_pattern": 'https://tw.appledaily.com/pf/api/v3/content/fetch/query-feed?query=%7B%22feedOffset%22%3A0%2C%22feedQuery%22%3A%22taxonomy.primary_section._id%3A%5C%22%2Frealtime%2Flocal%5C%22%2BAND%2Btype%3Astory%2BAND%2Bpublish_date%3A%5Bnow-48h%2Fh%2BTO%2Bnow%5D%22%2C%22feedSize%22%3A%22100%22%2C%22sort%22%3A%22display_date%3Adesc%22%7D&d={}&_website=tw-appledaily',
-            "url": 'https://tw.appledaily.com/realtime/local/',
+    requests = [
+        {
+            "media": "appledaily",
+            "name": "appledaily",
+            "scrapy_key": "appledaily:start_urls",
+            "url_pattern": "https://tw.appledaily.com/pf/api/v3/content/fetch/query-feed?query=%7B%22feedOffset%22%3A0%2C%22feedQuery%22%3A%22taxonomy.primary_section._id%3A%5C%22%2Frealtime%2Flocal%5C%22%2BAND%2Btype%3Astory%2BAND%2Bpublish_date%3A%5Bnow-48h%2Fh%2BTO%2Bnow%5D%22%2C%22feedSize%22%3A%22100%22%2C%22sort%22%3A%22display_date%3Adesc%22%7D&d={}&_website=tw-appledaily",
+            "url": "https://tw.appledaily.com/realtime/local/",
+            "priority": 1,
+            "enabled": True,
             "interval": 3600 * 2,
             "days_limit": 3600 * 24 * 2,
-            'enabled': True,
+        },
+        {
+            'media': "appledaily",
+            'name': "appledaily",
+            "scrapy_key": "appledaily:start_urls",
+            "url": "https://tw.appledaily.com/daily/headline/",
+            "url_pattern": "https://tw.appledaily.com/pf/api/v3/content/fetch/query-feed?query=%7B%22feedOffset%22%3A0%2C%22feedQuery%22%3A%22taxonomy.primary_section._id%3A%5C%22%2Fdaily%2Fheadline%5C%22%2BAND%2Btype%3Astory%2BAND%2Beditor_note%3A%5C%22{}%5C%22%2BAND%2BNOT%2Btaxonomy.tags.text.raw%3A_no_show_for_web%2BAND%2BNOT%2Btaxonomy.tags.text.raw%3A_nohkad%22%2C%22feedSize%22%3A100%2C%22sort%22%3A%22location%3Aasc%22%7D&d={}&_website=tw-appledaily", #feedsize來調整一頁看多少篇
             "priority": 1,
-        },{
-        'media': 'appledaily',
-            'name': 'appledaily',
-            'scrapy_key': 'appledaily:start_urls',
-            "url_pattern": 'https://tw.appledaily.com/pf/api/v3/content/fetch/query-feed?query=%7B%22feedOffset%22%3A0%2C%22feedQuery%22%3A%22taxonomy.primary_section._id%3A%5C%22%2Frealtime%2Flocal%5C%22%2BAND%2Btype%3Astory%2BAND%2Bpublish_date%3A%5Bnow-48h%2Fh%2BTO%2Bnow%5D%22%2C%22feedSize%22%3A%22100%22%2C%22sort%22%3A%22display_date%3Adesc%22%7D&d={}&_website=tw-appledaily','url':"https://tw.appledaily.com/daily/headline/"
+            "enabled": True,
             "interval": 3600 * 2,
-            "days_limit": 3600 * 24 * 2,
-            'enabled': True,
+            "days_limit": 3600 * 24 * 2, 
+        },
+        {
+            "media": "appledaily",
+            "name": "appledaily_keywords",
+            "scrapy_key": "appledaily_keywords:start_urls",
+            "url":"https://tw.appledaily.com/daily/headline/",
+            "url_pattern":"https://tw.appledaily.com/pf/api/v3/content/fetch/search-query?query=%7B%22searchTerm{}start%22%3A{}%7D&d={}&_website=tw-appledaily",
+            "keywords_list": ['吸金','地下通匯','洗錢','賭博','販毒','走私','仿冒','犯罪集團','侵占','背信','內線交易','行賄','詐貸','詐欺','貪汙','逃稅'],
+            "page": 0,
             "priority": 1,
-    }]
+            "enabled": True,
+            "interval": 3600 * 2,
+            "days_limit": 3600 * 24 * 2, 
+        }
+    ]
     for req in requests:
         yield req
 
 
-def save_to_redis(media):
-    redis_key = "{}:start_urls".format(media)
+def save_to_redis():
     password = SETTINGS['REDIS_PASSWORD']
     r = redis.StrictRedis(password=password)
-    q = RedisPriorityQueue(r, redis_key, encoding=ujson)
     for d in get_config():
+        q = RedisPriorityQueue(r, d['scrapy_key'], encoding=ujson)
         q.push(d, d['priority'])
 
 
 def save_to_mongo(media):
-    # m = pymongo.MongoClient(SETTINGS['MONGODB_SERVER'], SETTINGS['MONGODB_PORT'])
     m = pymongo.MongoClient('mongodb://%s:%s@%s'%(SETTINGS['MONGODB_USER'],SETTINGS['MONGODB_PASSWORD'],'localhost'))
     db = m['config']
     collection = db['urls']
@@ -59,7 +74,7 @@ if __name__ == '__main__':
     my_parser.add_argument('-a')
     args = my_parser.parse_args()
     if 'run' == args.a:
-        save_to_redis(media)
+        save_to_redis()
     elif 'save' == args.a:
         save_to_mongo(media)
     else:
--- info-scraper-master/scraper/scraper/spiders/appledaily_keyword.py
+++ info-scraper-master/scraper/scraper/spiders/appledaily_keyword.py
@@ -9,79 +9,120 @@ import re
 import json
 from .redis_spiders import RedisSpider
 
-# class AppledailySpider(RedisSpider):
-class Appledaily_keywordSpider(scrapy.Spider):
-    name = 'appledaily_keyword'
+# class Appledaily_keywordsSpider(RedisSpider):
+class Appledaily_keywordsSpider(scrapy.Spider):
+    name = 'appledaily_keywords'
 
     def start_requests(self):
 
         if isinstance(self, RedisSpider):
             return
-
-        requests = []
-        #關鍵字
-        key_dict = {'吸金':'%22%3A%22%25E5%2590%25B8%25E9%2587%2591%22%2C%22','地下通匯':'%22%3A%22%25E5%259C%25B0%25E4%25B8%258B%25E9%2580%259A%25E5%258C%25AF%22%2C%22','洗錢':'%22%3A%22%25E6%25B4%2597%25E9%258C%25A2%22%2C%22','賭博':'%22%3A%22%25E8%25B3%25AD%25E5%258D%259A%22%2C%22','販毒':'%22%3A%22%25E8%25B2%25A9%25E6%25AF%2592%22%2C%22','走私':'%22%3A%22%25E8%25B5%25B0%25E7%25A7%2581%22%2C%22','仿冒':'%22%3A%22%25E4%25BB%25BF%25E5%2586%2592%22%2C%22','犯罪集團':'%22%3A%22%25E7%258A%25AF%25E7%25BD%25AA%25E9%259B%2586%25E5%259C%2598%22%2C%22','侵占':'%22%3A%22%25E4%25BE%25B5%25E4%25BD%2594%22%2C%22','背信':'%22%3A%22%25E8%2583%258C%25E4%25BF%25A1%22%2C%22','內線交易':'%22%3A%22%25E5%2585%25A7%25E7%25B7%259A%25E4%25BA%25A4%25E6%2598%2593%22%2C%22','行賄':'%22%3A%22%25E8%25A1%258C%25E8%25B3%2584%22%2C%22','詐貸':'%22%3A%22%25E8%25A9%2590%25E8%25B2%25B8%22%2C%22','詐欺':'%22%3A%22%25E8%25A9%2590%25E6%25AC%25BA%22%2C%22','貪汙':'%22%3A%22%25E8%25B2%25AA%25E6%25B1%25A1%22%2C%22','逃稅':'%22%3A%22%25E9%2580%2583%25E7%25A8%2585%22%2C%22'}
-        for key in key＿dict.values():
-            for i in range(0,100,20):
-                api_pattern = 'https://tw.appledaily.com/pf/api/v3/content/fetch/search-query?query=%7B%22searchTerm{}start%22%3A{}%7D&d=170&_website=tw-appledaily'
-                api_pattern = api_pattern.format(key,str(i))
-                item = {"media": "appledaily",
-                    "name": "",
-                    "enabled": True,
-                    "days_limit": 3600 * 24 * 5,
-                    "interval": 3600 * 2,
-                    "url":"https://tw.appledaily.com",
-                    "url_pattern":api_pattern,
-                    "scrapy_key": "appledaily:start_urls",
-                    "priority": 1}
-                requests.append(item)
-
+       
+        requests = [
+            {
+                "media": "appledaily",
+                "name": "appledaily_keywords",
+                "scrapy_key": "appledaily_keywords:start_urls",
+                "url":"https://tw.appledaily.com/daily/headline/",
+                "url_pattern":"https://tw.appledaily.com/pf/api/v3/content/fetch/search-query?query=%7B%22searchTerm{}start%22%3A{}%7D&d={}&_website=tw-appledaily",
+                "keywords_list": ['吸金','地下通匯','洗錢','賭博','販毒','走私','仿冒','犯罪集團','侵占','背信','內線交易','行賄','詐貸','詐欺','貪汙','逃稅'],
+                "page": 0,
+                "priority": 1,
+                "enabled": True,
+                "days_limit": 3600 * 24 * 2, 
+                "interval": 3600 * 2,
+            }
+        ]
+           
         for request in requests:
-            yield scrapy.Request(request['url_pattern'],
+            yield scrapy.Request(request['url'],
                     meta=request,
+                    dont_filter=True,
+                    callback=self.parse)
+        
+    def parse(self,response):
+        meta = response.meta
+        soup = BeautifulSoup(response.body, 'html.parser')
+        
+        fusion_engine_script = soup.find('script',{'id':'fusion-engine-script'})
+        d = fusion_engine_script['src'].split('?d=')[-1]
+        
+        #關鍵字
+        key_dict = {'吸金':'%22%3A%22%25E5%2590%25B8%25E9%2587%2591%22%2C%22',
+                    '地下通匯':'%22%3A%22%25E5%259C%25B0%25E4%25B8%258B%25E9%2580%259A%25E5%258C%25AF%22%2C%22',
+                    '洗錢':'%22%3A%22%25E6%25B4%2597%25E9%258C%25A2%22%2C%22',
+                    '賭博':'%22%3A%22%25E8%25B3%25AD%25E5%258D%259A%22%2C%22',
+                    '販毒':'%22%3A%22%25E8%25B2%25A9%25E6%25AF%2592%22%2C%22',
+                    '走私':'%22%3A%22%25E8%25B5%25B0%25E7%25A7%2581%22%2C%22',
+                    '仿冒':'%22%3A%22%25E4%25BB%25BF%25E5%2586%2592%22%2C%22',
+                    '犯罪集團':'%22%3A%22%25E7%258A%25AF%25E7%25BD%25AA%25E9%259B%2586%25E5%259C%2598%22%2C%22',
+                    '侵占':'%22%3A%22%25E4%25BE%25B5%25E4%25BD%2594%22%2C%22',
+                    '背信':'%22%3A%22%25E8%2583%258C%25E4%25BF%25A1%22%2C%22',
+                    '內線交易':'%22%3A%22%25E5%2585%25A7%25E7%25B7%259A%25E4%25BA%25A4%25E6%2598%2593%22%2C%22',
+                    '行賄':'%22%3A%22%25E8%25A1%258C%25E8%25B3%2584%22%2C%22',
+                    '詐貸':'%22%3A%22%25E8%25A9%2590%25E8%25B2%25B8%22%2C%22',
+                    '詐欺':'%22%3A%22%25E8%25A9%2590%25E6%25AC%25BA%22%2C%22',
+                    '貪汙':'%22%3A%22%25E8%25B2%25AA%25E6%25B1%25A1%22%2C%22',
+                    '逃稅':'%22%3A%22%25E9%2580%2583%25E7%25A8%2585%22%2C%22'}
+
+        for key_name in meta['keywords_list']:
+            meta.update({'d':d})
+            meta.update({'key':key_dict[key_name]})
+            yield scrapy.Request(meta['url_pattern'].format(meta['key'], str(meta['page']), meta['d']),
+                    meta=meta,
                     dont_filter=True,
                     callback=self.parse_list)
     
     def parse_list(self, response):
         meta = response.meta
-        now = datetime.now()
-        past = now - timedelta(seconds=meta['days_limit'])
-        for i in range(0,20):
-            result = json.loads(response.body)
+        result = json.loads(response.body)
+
+        past = datetime.now() - timedelta(seconds=meta['days_limit'])
+
+        page_len = len(result['content'])
+        page = meta['page'] + page_len
+        meta.update({'page':page})
+
+        for i in range(0,page_len):
             url = result['content'][i]['sharing']['url']
             img = result['content'][i]['sharing']['image']
             time = result['content'][i]['issueId']
-            #print(time)
             category = result['content'][i]['brandCategoryName']
-            #print(category)
+
             meta.update({'category_name':category})
-
             meta.update({'img':img})
-            time = datetime.strptime(time, '%Y%m%d')
 
+            time = datetime.strptime(time, '%Y%m%d')
             if (time < past):
                 return
+
             yield scrapy.Request(url,
                     callback=self.parse_article,
                     meta=meta)
 
+        next_page = meta['url_pattern'].format(meta['key'], str(meta['page']), meta['d']) 
+        yield scrapy.Request(next_page,
+                meta=meta,
+                dont_filter=True,
+                callback=self.parse_list)
+
     def parse_article(self, response):
         soup = BeautifulSoup(response.body, 'html.parser')
         meta = response.meta
+        content = self.parse_content(soup)
+
         item = NewsItem()
         item['url'] = response.url
-        item['author'] = []
+        item['author'] = self.parse_author(content)
         item['article_title'] = self.parse_title(soup)
-        #print(item['article_title'])
         item['author_url'] = []
         item['comment'] = []
         item['date'] = self.parse_datetime(soup)
-        #print(item['date'])
         item['metadata'] = self.parse_metadata(soup,meta)
-        item['content'] = self.parse_content(soup)
+        item['content'] = content
         item['content_type'] = 0
         item['media'] = 'appledaily'
-        item['proto'] = 'APPLEDAILY_PARSE_ITEM'
+        item['proto'] = 'APPLEDAILY_KEYWORDS_PARSE_ITEM'
         return item
 
     def parse_datetime(self, soup):
@@ -95,7 +136,7 @@ class Appledaily_keywordSpider(scrapy.Spider):
     
     
     def parse_title(self, soup):
-        title = soup.find('h2',{'class','text_medium'}).text
+        title = soup.find('div',{'class':'article__header'}).find('span').text
         return title
     
     def parse_content(self, soup):
@@ -105,9 +146,6 @@ class Appledaily_keywordSpider(scrapy.Spider):
         return articlebody
 
     def parse_metadata(self, soup,meta):
-
-        #keywords = soup.find('div','article-hash-tag').find_all('span','hash-tag')
-        #keywords = [x.text.replace('#','') for x in keywords]
         category = meta['category_name']
         try:
             image_url = meta['img']
@@ -116,3 +154,19 @@ class Appledaily_keywordSpider(scrapy.Spider):
         metadata = {'category':category, 'image_url':image_url}
         return metadata
     
+    def parse_author(self, content):
+        content = content.replace('(','（')
+        content = content.replace(')','）')
+        re_pattern = ["（(.{,20})／(.{,5})報導）", "【(.{,20})／(.{,5})報導】"]
+        re_rslt = []
+
+        for p in re_pattern:
+            search_rslt = re.search(p, content)
+            if search_rslt != None:
+                re_rslt.append(search_rslt.group(1)) 
+
+        if re_rslt!=[]:
+            author = re_rslt[0]
+        else:
+            author = ''
+        return author
